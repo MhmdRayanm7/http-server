a7afb2ca1fa9ff0dc064c169e6a328c29adc9b3a
Mhmd Rayan <m7mddotm7@gmail.com>
2026-08-14 15:15:10 +0300

    chore: add code comment

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -3,6 +3,6 @@ import { config } from "../config.js";
 
 
 export function handlerMetrics(req: Request, res: Response) {
-  res.set("Content-Type", "text/plain");
+  res.set("Content-Type", "text/plain"); // (not necessary)  automatically detects the type of data passed into it and sets an appropriate
   res.send(`Hits: ${config.fileserverHits}`);
 }
