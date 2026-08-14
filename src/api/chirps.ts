5976508b54ddd7a9e8423604bcc00cf5ebd0f444
Mhmd Rayan <m7mddotm7@gmail.com>
2026-08-14 18:09:59 +0300

    feat(api): add endpoint to validate chirp length

---
+++ src/api/chirps.ts
@@ -0,0 +1,30 @@
+import type { Request, Response } from "express";
+
+export function handlerChirpsValidate(req: Request, res: Response) {
+  let body = "";
+
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  // 3. Listen for end events
+  req.on("end", () => {
+    try {
+      const parsedBody = JSON.parse(body);
+
+      if (parsedBody.body.length > 140) {
+        res.status(400).json({
+          error: "Chirp is too long",
+        });
+        return;
+      }
+      res.status(200).json({
+        valid: true,
+      });
+    } catch {
+      res.status(400).json({
+        error: "Something went wrong",
+      });
+    }
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import {
 } from "./api/middleware.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
+import { handlerChirpsValidate } from "./api/chirps.js";
 
 const app = express();
 const PORT = 8080;
@@ -19,6 +20,8 @@ app.get("/api/healthz", handlerReadiness);
 app.get("/admin/metrics", handlerMetrics);
 
 app.post("/admin/reset", handlerReset );
+
+app.post("/api/validate_chirp", handlerChirpsValidate );
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
