8e63eff199a9128cc5dab576bd47f2b9c223b3f7
Mhmd Rayan <m7mddotm7@gmail.com>
2026-08-15 13:25:05 +0300

    feat: add drizzle queries and db connection

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,22 @@
+process.loadEnvFile();
+
+
 type APIConfig = {
   fileserverHits: number;
+  dbURL : string;
 };
+
+function envOrThrow(key: string): string {
+  const value = process.env[key];
+
+  if (!value) {
+    throw new Error(`Environment variable ${key} is missing`);
+  }
+
+  return value;
+}
 
 export const config : APIConfig = {
     fileserverHits: 0,
+    dbURL:envOrThrow("DB_URL"),
 };
