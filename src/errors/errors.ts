46b054d7d4c5fa01631ed71154235dd0f0f17d68
Mhmd Rayan <m7mddotm7@gmail.com>
2026-08-15 16:05:02 +0300

    docs : add comments

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,21 +1,21 @@
-export class BadRequestError extends Error {
+export class BadRequestError extends Error { //400
   constructor(message: string) {
     super(message);
   }
 }
-export class UnauthorizedError extends Error {
-  constructor(message: string) {
-    super(message);
-  }
-}
-
-export class ForbiddenError extends Error {
+export class UnauthorizedError extends Error { //401
   constructor(message: string) {
     super(message);
   }
 }
 
-export class NotFoundError extends Error {
+export class ForbiddenError extends Error { //403
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class NotFoundError extends Error { //404
   constructor(message: string) {
     super(message);
   }
