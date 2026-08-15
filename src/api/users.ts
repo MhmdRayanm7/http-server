ea95e61ec8a6958c93bbe2fd7488e00036c6c8f0
Mhmd Rayan <m7mddotm7@gmail.com>
2026-08-15 16:11:15 +0300

    feat(users-api): add validation and error handling

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,12 +1,24 @@
 import type { Request, Response } from "express";
 import { createUser } from "../db/queries/users.js";
+import { BadRequestError } from "../errors/errors.js";
 
 export async function handlerUsersCreate(req: Request, res: Response) {
-  const email = req.body.email;
+  type parameters = {
+    email: string;
+  };
+  const params: parameters = req.body;
+
+  if (!params.email) {
+    throw new BadRequestError("Missing required fields");
+  }
 
   const user = await createUser({
-    email: email,
+    email: params.email,
   });
-  
+
+  if (!user) {
+    throw new Error("Could not create user");
+  }
+
   res.status(201).json(user);
 }
